8a1e7916054d42ade9a4733e08a0065ca3480464
hakobya4 <108638724+hakobya4@users.noreply.github.com>
2023-08-23 17:02:39 -0400

    Added password hashing, user validation and Heroku hosting

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express'),
   bodyParser = require('body-parser'),
-  uuid = require('uuid');
   morgan = require('morgan'),
   fs = require('fs'),
   path = require('path');
   mongoose = require('mongoose');
   Models = require('./models.js');
+  cors = require('cors');
+
+const { check, validationResult } = require('express-validator');
+
 
 const Movies = Models.Movie;
 const Users = Models.User;
@@ -13,9 +16,19 @@ const Users = Models.User;
 mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
@@ -102,7 +115,22 @@ app.get('/movies/:title/director', passport.authenticate('jwt', { session: false
 );
 
 // Adds register user using username, date of birth, email and password
-app.post('/users', passport.authenticate('jwt', { session: false }), async (req, res) => {
+app.post('/users', passport.authenticate('jwt', { session: false }),
+  [
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], async (req, res) => {
+
+  // check the validation object for errors
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+  let hashedPassword = Users.hashPassword(req.body.Password);
   await Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
@@ -111,7 +139,7 @@ app.post('/users', passport.authenticate('jwt', { session: false }), async (req,
         Users
           .create({
             Username: req.body.Username,
-            Password: req.body.Password,
+            Password: hashedPassword,
             Email: req.body.Email,
             Birthday: req.body.Birthday
           })
@@ -242,6 +270,7 @@ app.delete('/users/:username', passport.authenticate('jwt', { session: false }),
     });
 });
 
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
